62d444524c547d71a1b6187e2b1a5d383e9f308e
MarceloFCandido <fcandido.marcelo@gmail.com>
2021-12-05 23:46:14 -0300

    doing exercise 9

--- js/bubbles.js
+++ js/bubbles.js
@@ -1,5 +1,5 @@
-const TEMPO_MINIMO = 1000;    // 1s em milissegundos
-const TEMPO_VARIAVEL = 3000;  // 3s
+const TEMPO_MINIMO = 200;    // 1s em milissegundos
+const TEMPO_VARIAVEL = 300;  // 3s
 const DURACAO = 5000;         // 5s
 const bolhaEl = carregaUmaImagem('images/bolha.png');
 
@@ -51,12 +51,6 @@ function criaUmaBolha() {
   setTimeout(criaUmaBolha, proximaBolhaDaqui);
 }
 
-
-
-
 criaUmaBolha();
-
-
-
 
 criaUmaBolha();
